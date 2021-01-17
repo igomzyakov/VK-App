12539355075a97e3cceee6f0630309c779b4651c
Igor <igomzyakov@gmail.com>
2021-01-17 22:42:25 +0300

    не получилось показать стрелочку кодом при переходе из таблицы в коллекцию не получилось применить метод свайпа кодом к таблвью (друзья)

--- VK App/VK App/LoginFormController.swift
+++ VK App/VK App/LoginFormController.swift
@@ -9,23 +9,31 @@ import UIKit
 
 class LoginFormController: UIViewController {
 
+    @IBAction func myUnwindAction(unwindSegue: UIStoryboardSegue){}
+
     @IBOutlet var scrollView: UIScrollView?
     @IBOutlet var loginTextField: UITextField!
     @IBOutlet var passowrdTextField: UITextField!
-    @IBAction func loginButtonPressed(_ sender: Any) {
-        // Получаем текст логина
+   
+    
+    //отлавливаем сегу перехода на таббарконтроллер
+     override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
                 let login = loginTextField.text!
-                // Получаем текст-пароль
                 let password = passowrdTextField.text!
-                
-                // Проверяем, верны ли они
                 if login == "1" && password == "1" {
-                    print("успешная авторизация")
+                    return true
                 } else {
-                    print("неуспешная авторизация")
+                    // Создаем контроллер
+                    let alert = UIAlertController(title: "Ошибка", message: "Введены неверные данные пользователя", preferredStyle: .alert)
+                    // Создаем кнопку для UIAlertController
+                    let action = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                    // Добавляем кнопку на UIAlertController
+                    alert.addAction(action)
+                    // Показываем UIAlertController
+                    present(alert, animated: true, completion: nil)
+                    return false
                 }
-
-    }
+            }
     
     /// Это свойство нужно для того, чтобы понимать отображена ли клавиатура в данный момент или нет, так как нотификации о ее появлении/убирании приходят не совсем корректно (баг системы)
     private var keyboardShown = false
